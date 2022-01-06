9c5b6ec33095d3a6f6a2422d471122462998e9d9
Mateusz Dregier <dregierm@gmail.com>
2022-01-06 18:31:00 +0100

    addFood added

--- src/snake.cpp
+++ src/snake.cpp
@@ -1,6 +1,48 @@
 #include "snake.h"
 #include "screen.h"
 #include <cstring>
+#include <cstdlib>
+#include <ctime>
+
+void CSnake::addFood()
+{
+    if((geom.size.x-2) * (geom.size.y-2) <= (int)snake.size()) return;
+    CPoint temp;
+
+    while (true)
+    {
+        temp = CPoint(rand() % (geom.size.x - 2) + 1, rand() % (geom.size.y - 2) + 1);
+        for(auto &s: snake)
+            if(!(s.x == temp.x && s.y == temp.y)) {
+                food = temp;
+                return;
+            }
+    }
+}
+
+void CSnake::reset()
+{
+    alive = true;
+    help = pause = false;
+    level = 0;
+    snake.clear();
+
+    snake.push_back(CPoint(4, 2)); //head *
+    snake.push_back(CPoint(3, 2)); //tail
+    snake.push_back(CPoint(2, 2)); //tail
+    addFood();
+}
+
+void CSnake::paint_snake()
+{
+    gotoyx(snake[0].y + geom.topleft.y, snake[0].x + geom.topleft.x);
+    printc('*');
+
+    for (unsigned int i = 1; i < snake.size(); i++) {
+        gotoyx(snake[i].y + geom.topleft.y, snake[i].x + geom.topleft.x);
+        printc('+');
+    }
+}
 
 void CSnake::paint_help()
 {
@@ -17,9 +59,25 @@ void CSnake::paint_help()
     printl("move window (in pause mode)");
 }
 
+CSnake::CSnake(CRect r, char _c): CFramedWindow(r, _c)
+{
+    srand(time(NULL));
+    reset();
+    help = true;
+}
+
 void CSnake::paint()
 {
     CFramedWindow::paint();
+
+    if(help)
+        paint_help();
+
+    if(!alive) {
+        gotoyx(geom.topleft.y + 1, geom.topleft.x + 1);
+        printl("GAME OVER, result: %d", level);
+        return;
+    }
 
     if(pause) {
         gotoyx(geom.topleft.y + geom.size.y/2, geom.topleft.x + geom.size.x/2 - strlen("**** PAUSE ****")/2);
@@ -32,9 +90,10 @@ void CSnake::paint()
     gotoyx(geom.topleft.y - 1, geom.topleft.x);
     printl("| LEVEL: %d |", level);
 
-    if(help)
-        paint_help();
+    paint_snake();
 
+    gotoyx(food.y + geom.topleft.y, food.x + geom.topleft.x);
+    printc('O');
 }
 
 bool CSnake::handleEvent(int c)
@@ -48,6 +107,12 @@ bool CSnake::handleEvent(int c)
         help = !help;
         return true;
     }
+
+   /* if(!pause) {
+        switch (key) {
+            case KEY_UP:
+        }
+    }*/
 
     return CFramedWindow::handleEvent(c);
 }
--- src/snake.h
+++ src/snake.h
@@ -2,17 +2,26 @@
 #define __SNAKE_H__
 
 #include "winsys.h"
+#include <iostream>
+#include <vector>
 
 class CSnake:public CFramedWindow
 {
 private:
-    int level = 0;
-    bool pause = false;
-    bool help = true;
+    int level;
+    bool pause;
+    bool help;
+    bool alive;
 
+    CPoint food;
+    std::vector <CPoint> snake;
+
+    void reset();
     void paint_help();
+    void paint_snake();
+    void addFood();
 public:
-    CSnake(CRect r, char _c = ' '): CFramedWindow(r, _c) {};
+    CSnake(CRect r, char _c = ' ');
 
     void paint();
     bool handleEvent(int c);
